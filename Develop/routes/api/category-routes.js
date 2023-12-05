fc139a403e8744e9766bc61b8e2bb7e3fff8994e
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-12-04 19:28:50 -0600

    get post for router for category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,6 +6,13 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll().then((categoryData) => {
+    res.status(200).json(categoryData);
+  })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+
 });
 
 router.get('/:id', (req, res) => {
