c73799b18a6c01cb0896769df54802cf0261ac99
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-12-04 19:35:36 -0600

    post method for new category to create one

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -33,6 +33,14 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+    .then((newCategory) => {
+      res.json(newCategory);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+
 });
 
 router.put('/:id', (req, res) => {
