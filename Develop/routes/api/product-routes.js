7a6a0c82286aac9e5a10ba3f73d0c28e9a36e1b0
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-12-04 19:19:17 -0600

    product routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,6 +7,12 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll().then((productData) => {
+    res.status(200).json(productData);
+  })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 // get one product
