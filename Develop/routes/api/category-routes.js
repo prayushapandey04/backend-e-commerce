f70ffcc7e485709b6dcbd4935f1ce065272ab2cd
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-12-04 19:38:07 -0600

    updating a category by its id value

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -45,6 +45,21 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedCategory) => {
+      res.json(updatedCateogry);
+    })
+    .catch((err) => res.json(err));
+
 });
 
 router.delete('/:id', (req, res) => {
